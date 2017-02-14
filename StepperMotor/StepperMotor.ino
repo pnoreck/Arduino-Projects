84feaefe9da4840c9d8703a0cfc93c37b92927f2
pnoreck <github@t3x.ch>
2017-02-14 21:02:45 +0100

    Stepper Motor Script added

---
+++ StepperMotor/StepperMotor.ino
@@ -0,0 +1,126 @@
+/*************************************************************
+Motor Shield Stepper Demo
+by Randy Sarafan
+
+For more information see:
+https://www.instructables.com/id/Arduino-Motor-Shield-Tutorial/
+
+*************************************************************/
+
+int delaylegnth = 20;
+
+/**
+ * SETUP Cables on motor shield:
+ * A+ = black
+ * A- = green
+ * B+ = red
+ * B- = blue
+ */
+void turn_left_four_steps(){
+ 
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(8, HIGH); //DISABLE CH B
+
+  digitalWrite(12, HIGH);   //Sets direction of CH A
+  analogWrite(3, 255);   //Moves CH A
+  
+  delay(delaylegnth);
+  
+  digitalWrite(9, HIGH);  //DISABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+
+  digitalWrite(13, LOW);   //Sets direction of CH B
+  analogWrite(11, 255);   //Moves CH B
+  
+  delay(delaylegnth);
+  
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(8, HIGH); //DISABLE CH B
+
+  digitalWrite(12, LOW);   //Sets direction of CH A
+  analogWrite(3, 255);   //Moves CH A
+  
+  delay(delaylegnth);
+    
+  digitalWrite(9, HIGH);  //DISABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+
+  digitalWrite(13, HIGH);   //Sets direction of CH B
+  analogWrite(11, 255);   //Moves CH B
+  
+  delay(delaylegnth);
+
+}
+
+
+void turn_right_four_steps(){
+ 
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(8, HIGH); //DISABLE CH B
+
+  digitalWrite(12, HIGH);   //Sets direction of CH A
+  analogWrite(3, 255);   //Moves CH A
+  
+  delay(delaylegnth);
+  
+  digitalWrite(9, HIGH);  //DISABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+
+  digitalWrite(13, HIGH);   //Sets direction of CH B
+  analogWrite(11, 255);   //Moves CH B
+  
+  delay(delaylegnth);
+  
+  digitalWrite(9, LOW);  //ENABLE CH A
+  digitalWrite(8, HIGH); //DISABLE CH B
+
+  digitalWrite(12, LOW);   //Sets direction of CH A
+  analogWrite(3, 255);   //Moves CH A
+  
+  delay(delaylegnth);
+    
+  digitalWrite(9, HIGH);  //DISABLE CH A
+  digitalWrite(8, LOW); //ENABLE CH B
+
+  digitalWrite(13, LOW);   //Sets direction of CH B
+  analogWrite(11, 255);   //Moves CH B
+  
+  delay(delaylegnth);
+
+}
+
+void turn_left(int steps) {
+  for(int x = 0; x < steps; x++) {
+    turn_left_four_steps();
+  }
+}
+
+void turn_right(int steps) {
+  for(int x = 0; x < steps; x++) {
+    turn_right_four_steps();
+  }
+}
+
+
+void setup() {
+  
+  //establish motor direction toggle pins
+  pinMode(12, OUTPUT); //CH A -- HIGH = forwards and LOW = backwards???
+  pinMode(13, OUTPUT); //CH B -- HIGH = forwards and LOW = backwards???
+  
+  //establish motor brake pins
+  pinMode(9, OUTPUT); //brake (disable) CH A
+  pinMode(8, OUTPUT); //brake (disable) CH B
+  
+}
+
+void loop(){
+  int x = 50;
+  
+  turn_left(x);
+  delay(500);
+  turn_right(x);
+  delay(2000);
+
+}
+
