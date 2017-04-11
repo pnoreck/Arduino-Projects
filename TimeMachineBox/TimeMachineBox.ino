6f26ba0c4536293b1dac6038a380c512ca39364b
pnoreck <github@t3x.ch>
2017-04-11 23:23:31 +0200

    * Debugging and verifying the circuit

--- TimeMachineBox/TimeMachineBox.ino
+++ TimeMachineBox/TimeMachineBox.ino
@@ -4,21 +4,34 @@
 // on a 7-segment-display
 //****************************************************************
 
-//Pin connected to ST_CP of 74HC595
-int latchPin = 8;
+//Pin connected to ST_CP of 74HC595 
+// Arduino Pin 8
+int latchPin = 2;
 //Pin connected to SH_CP of 74HC595
-int clockPin = 12;
+// Arduino Pin 12
+int clockPin = 3;
 ////Pin connected to DS of 74HC595
-int dataPin = 11;
+// Arduino 11
+int dataPin = 0;
 
+// Pin connected to the transistor / output switch
+int lightSwitch = 1;
+boolean lightOn = false;
+
+// Pin connected to the light sensor
+#define lightSensor A2
+
+int currentDisplayNumber = 0;
 int blinkDelay = 500;
+int lightValue = 0;
 
 void setup() {
   //set pins to output so you can control the shift register
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
-  Serial.begin(9600);
+  pinMode(lightSwitch, OUTPUT);
+  pinMode(lightSensor, INPUT);
 }
 
 int lut[] = {
@@ -63,15 +76,14 @@ int lut[] = {
   198,  // °
   148,  // burger
 
-
+  // 35 = empty
+  0,
 };
 
 
 void shiftToDisplay(int mapKey) {
 
   int numberToDisplay = lut[mapKey];
-  Serial.println(mapKey);
-  Serial.println(numberToDisplay);
 
   // take the latchPin low so
   // the LEDs don't change while you're sending in bits:
@@ -97,9 +109,58 @@ void blinkNumber(int number, int blinkingTimes) {
 
 
 void loop() {
-  for (int number = 0; number <= 9; number++) {
-    blinkNumber(number, 60);
+  
+  lightValue = 1024 - analogRead(lightSensor);
+  /*
+  if(lightValue >= 900) {
+    number = 9;
+  } else if(lightValue >= 800 && lightValue < 900) {
+    number = 8;
+  } else if(lightValue >= 700 && lightValue < 800) {
+    number = 7;
+  } else if(lightValue >= 600 && lightValue < 700) {
+    number = 6;
+  } else if(lightValue >= 500 && lightValue < 600) {
+    number = 5;
+  } else if(lightValue >= 400 && lightValue < 500) {
+    number = 4;
+  } else if(lightValue >= 300 && lightValue < 400) {
+    number = 3;
+  } else if(lightValue >= 200 && lightValue < 300) {
+    number = 2;
+  } else if(lightValue >= 100 && lightValue < 200) {
+    number = 1;
+  } else {
+    number = 0;
   }
-  delay(500);
+  */
+  
+  if(lightValue < 300 && currentDisplayNumber < 0) {
+    currentDisplayNumber = 9;
+    lightOn = true;
+  }
+  else if(lightValue > 900 && currentDisplayNumber < 7) {
+    currentDisplayNumber = 9;
+    lightOn = true;
+  }
+  else if(lightValue > 900 && currentDisplayNumber >= 8) {
+    currentDisplayNumber = -1;
+    lightOn = false;
+  }
+  else if(lightValue < 0) {
+    lightOn = false;
+  }
+  
+  if(currentDisplayNumber >= 0 && currentDisplayNumber < 10) {
+    // blinkNumber(currentDisplayNumber, 60);
+    blinkNumber(currentDisplayNumber, 2);
+    digitalWrite(lightSwitch, lightOn);
+    currentDisplayNumber = currentDisplayNumber - 1;
+  }
+  else {
+    shiftToDisplay(35);
+  }
+  
+  delay(100);
 }
 
