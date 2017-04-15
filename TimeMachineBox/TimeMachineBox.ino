230fc5dc6ffb3c1f28e48740fbf6e469231d0022
pnoreck <github@t3x.ch>
2017-04-15 11:50:40 +0200

    * Debugging and verifying the circuit

--- TimeMachineBox/TimeMachineBox.ino
+++ TimeMachineBox/TimeMachineBox.ino
@@ -4,34 +4,65 @@
 // on a 7-segment-display
 //****************************************************************
 
-//Pin connected to ST_CP of 74HC595 
+//Pin connected to ST_CP of 74HC595
 // Arduino Pin 8
 int latchPin = 2;
+
 //Pin connected to SH_CP of 74HC595
 // Arduino Pin 12
 int clockPin = 3;
+
 ////Pin connected to DS of 74HC595
 // Arduino 11
 int dataPin = 0;
 
 // Pin connected to the transistor / output switch
 int lightSwitch = 1;
+
+
+
+void setArduinoPins() {
+
+  //Pin connected to ST_CP of 74HC595
+  // Arduino Pin 8
+  latchPin = 8;
+
+  //Pin connected to SH_CP of 74HC595
+  // Arduino Pin 12
+  clockPin = 12;
+
+  ////Pin connected to DS of 74HC595
+  // Arduino 11
+  dataPin = 11;
+
+  // Pin connected to the transistor / output switch
+  lightSwitch = 7;
+
+  // Serial.begin(9600);
+}
+
 boolean lightOn = false;
 
 // Pin connected to the light sensor
 #define lightSensor A2
 
-int currentDisplayNumber = 0;
+int currentDisplayNumber = -1;
 int blinkDelay = 500;
-int lightValue = 0;
+int lastLightValue = 0;
+bool lastRun = false;
 
 void setup() {
+
+  // setArduinoPins();
+
   //set pins to output so you can control the shift register
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(lightSwitch, OUTPUT);
   pinMode(lightSensor, INPUT);
+
+  delay(1000);
 }
 
 int lut[] = {
@@ -98,10 +129,10 @@ void shiftToDisplay(int mapKey) {
 
 void blinkNumber(int number, int blinkingTimes) {
   for (int i = 0; i < blinkingTimes; i++) {
-    shiftToDisplay(9 - number);
+    shiftToDisplay(number);
     // pause before next value:
     delay(blinkDelay);
-    shiftToDisplay(19 - number);
+    shiftToDisplay(10 + number);
     // pause before next value:
     delay(blinkDelay);
   }
@@ -109,58 +140,75 @@ void blinkNumber(int number, int blinkingTimes) {
 
 
 void loop() {
-  
-  lightValue = 1024 - analogRead(lightSensor);
+
+  int lightValue = analogRead(lightSensor);
   /*
-  if(lightValue >= 900) {
+    if(lightValue >= 900) {
     number = 9;
-  } else if(lightValue >= 800 && lightValue < 900) {
+    } else if(lightValue >= 800 && lightValue < 900) {
     number = 8;
-  } else if(lightValue >= 700 && lightValue < 800) {
+    } else if(lightValue >= 700 && lightValue < 800) {
     number = 7;
-  } else if(lightValue >= 600 && lightValue < 700) {
+    } else if(lightValue >= 600 && lightValue < 700) {
     number = 6;
-  } else if(lightValue >= 500 && lightValue < 600) {
+    } else if(lightValue >= 500 && lightValue < 600) {
     number = 5;
-  } else if(lightValue >= 400 && lightValue < 500) {
+    } else if(lightValue >= 400 && lightValue < 500) {
     number = 4;
-  } else if(lightValue >= 300 && lightValue < 400) {
+    } else if(lightValue >= 300 && lightValue < 400) {
     number = 3;
-  } else if(lightValue >= 200 && lightValue < 300) {
+    } else if(lightValue >= 200 && lightValue < 300) {
     number = 2;
-  } else if(lightValue >= 100 && lightValue < 200) {
+    } else if(lightValue >= 100 && lightValue < 200) {
     number = 1;
-  } else {
+    } else {
     number = 0;
-  }
+    }
   */
-  
-  if(lightValue < 300 && currentDisplayNumber < 0) {
+
+  if (lightValue <= 500 && currentDisplayNumber < 0 && lastRun == false) {
+    currentDisplayNumber = 9;
+    lightOn = true;
+    lastRun = true;
+  }
+  else if (lightValue > 900 && currentDisplayNumber < 7 && currentDisplayNumber >= 0) {
     currentDisplayNumber = 9;
     lightOn = true;
   }
-  else if(lightValue > 900 && currentDisplayNumber < 7) {
-    currentDisplayNumber = 9;
-    lightOn = true;
-  }
-  else if(lightValue > 900 && currentDisplayNumber >= 8) {
+  else if (lightValue > 900 && currentDisplayNumber >= 8) {
     currentDisplayNumber = -1;
     lightOn = false;
+    lastRun = false;
   }
-  else if(lightValue < 0) {
+  else if (lightValue > 500 && lastRun == true) {
+    lastRun = false;
     lightOn = false;
   }
+  else if (currentDisplayNumber < 0) {
+    lightOn = false;
+  }
+
+  /*
+    Serial.println("Light value:");
+    Serial.println(lightValue);
+    Serial.println("current number");
+    Serial.println(currentDisplayNumber);
+  */
   
-  if(currentDisplayNumber >= 0 && currentDisplayNumber < 10) {
+  digitalWrite(lightSwitch, lightOn);
+
+  if (currentDisplayNumber >= 0 && currentDisplayNumber < 10) {
     // blinkNumber(currentDisplayNumber, 60);
     blinkNumber(currentDisplayNumber, 2);
-    digitalWrite(lightSwitch, lightOn);
     currentDisplayNumber = currentDisplayNumber - 1;
+    if (currentDisplayNumber < 0) {
+
+    }
   }
   else {
     shiftToDisplay(35);
+    delay(1000);
   }
-  
-  delay(100);
+
 }
 
